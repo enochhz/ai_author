0e15c48236aeb7486f0326371c2884a5c010429b
enochhz <walkerhaozheng@gmail.com>
2019-06-13 23:54:12 -0700

    update the script file

--- textgenrnn/textgenrnn_script.py
+++ textgenrnn/textgenrnn_script.py
@@ -1,5 +1,6 @@
 from textgenrnn import textgenrnn
-textgen = textgenrnn(weights_path='shakespeare_weights.hdf5', vocab_path='shakespeare_vocab.json', config_path='shakespeare_config.json')
+import sys
+textgen = textgenrnn(weights_path=sys.argv[1], vocab_path=sys.argv[2], config_path=sys.argv[3])
 
 textgen.generate_samples(n=1, max_gen_length=300)
 textgen.generate_to_file('shakespeare_texts.txt', n=1, max_gen_length=300)
